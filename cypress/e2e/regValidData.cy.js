adb11169298f64b3a15cb8d11bd25f02726c2aba
Patryk Czarnecki <papacarlito2@gmail.com>
2024-07-10 12:22:40 +0200

    asd

--- cypress/e2e/regValidData.cy.js
+++ cypress/e2e/regValidData.cy.js
@@ -1,1 +1,43 @@
+import base from "../pages/base";
+import regValid from "../pages/regValid";
+import { correctEmail, correctPass, correctFirstName, correctLastName, correctTelNumber } from "../fixtures/regData.json";
+const { reportTestResult } = require('../support/testlink');
 
+describe("User Registration Process", () => {
+    beforeEach(() => {
+        cy.clearCookies();
+    });
+
+    it("Registration with Valid Data", () => {
+        const testCaseId = '59';
+        const testPlanId = '151';
+        const buildId = '1';
+        let status = 'f'; // domyślnie ustaw na "f"
+        const notes = 'Test passed successfully';
+
+        base.openHomePage();
+        base.cookies.click();
+        base.login.click();
+        base.reg.click();
+
+        const { regEmail, regPass, firstName, lastName, telNumber, privCheck } = regValid;
+
+        regEmail.type(correctEmail);
+        regPass.type(correctPass);
+        firstName.type(correctFirstName);
+        lastName.type(correctLastName);
+        telNumber.type(correctTelNumber);        
+        privCheck.check({ force: true }).should('be.checked');
+
+        // Ustalanie statusu na podstawie wyniku testu
+        cy.get('input[data-qa-input-qualifier="firstName"]').invoke('val').then(value => {
+            if (value === correctFirstName) {
+                status = 'p'; // status 'p' dla pozytywnego wyniku
+            }
+        }).then(() => {
+            // Wywołanie reportTestResult po zakończeniu sprawdzania stanu
+            reportTestResult(testCaseId, testPlanId, buildId, status, notes);
+        });
+        firstName.should("have.value", correctFirstName);
+    });
+});
